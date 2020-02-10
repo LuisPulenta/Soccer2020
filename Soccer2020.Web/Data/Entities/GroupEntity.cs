6e3372fcd67469c343ec380bbc008f392b20a149
Luis Nuñez <luisalbertonu@gmail.com>
2020-02-10 13:35:53 -0300

    GroupEntity

---
+++ Soccer2020.Web/Data/Entities/GroupEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Soccer2020.Web.Data.Entities
+{
+    public class GroupEntity
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Grupo")]
+        [MaxLength(30, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        public string Name { get; set; }
+
+        public TournamentEntity Tournament { get; set; }
+    }
+}
---
+++ Soccer2020.Web/Data/Entities/TournamentEntity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Soccer2020.Web.Data.Entities
+{
+         public class TournamentEntity
+        {
+            public int Id { get; set; }
+
+            [Display(Name = "Torneo")]
+            [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres.")]
+            [Required(ErrorMessage = "El campo {0} es requerido.")]
+            public string Name { get; set; }
+
+            [DataType(DataType.DateTime)]
+            [Display(Name = "Fecha Inicio")]
+            [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = false)]
+            public DateTime StartDate { get; set; }
+
+            [Display(Name = "Fecha Inicio")]
+            [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = false)]
+            public DateTime StartDateLocal => StartDate.ToLocalTime();
+
+            [DataType(DataType.DateTime)]
+            [Display(Name = "Fecha Fin")]
+            [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = false)]
+            public DateTime EndDate { get; set; }
+
+            [Display(Name = "Fecha Fin")]
+            [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = false)]
+            public DateTime EndDateLocal => EndDate.ToLocalTime();
+
+            [Display(Name = "Está Activo?")]
+            public bool IsActive { get; set; }
+
+            [Display(Name = "Logo")]
+            public string LogoPath { get; set; }
+
+            public ICollection<GroupEntity> Groups { get; set; }
+    }   
+}
