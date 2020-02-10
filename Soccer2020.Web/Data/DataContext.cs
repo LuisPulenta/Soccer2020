7191ce65f4fc7706217024cc49d70d2f3e554428
Luis Nuñez <luisalbertonu@gmail.com>
2020-02-10 12:33:31 -0300

    TeamsController

---
+++ Soccer2020.Web/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Soccer2020.Web.Data.Entities;
+
+namespace Soccer2020.Web.Data
+{
+    public class DataContext : DbContext
+    {
+
+        #region Constructor
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        #endregion
+
+        public DbSet<TeamEntity> Teams { get; set; }
+    }
+}
---
+++ Soccer2020.Web/Data/Entities/TeamEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Soccer2020.Web.Data.Entities
+{
+    public class TeamEntity
+    {
+        public int Id { get; set; }
+        
+        [Display(Name = "Equipo")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        public string Name { get; set; }
+
+        [Display(Name = "Logo")]
+        public string LogoPath { get; set; }
+    }
+}
